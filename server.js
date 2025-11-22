b3dfcc21cc0de1910b5a9d826409a19f4922c0d8
OGATALOX <ahmed1238abc8@gmail.com>
2025-11-21 22:31:23 -0500

    server.js

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ const bcrypt = require('bcrypt');
 const session = require('express-session');
 const SQLiteStore = require('connect-sqlite3')(session);
 
+// --- MONGODB (Atlas) ---
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✓ Connected to MongoDB"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
