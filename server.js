7865f004fa3628823df67f6c1b76add09fe17ff4
OGATALOX <ahmed1238abc8@gmail.com>
2025-11-22 00:58:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -25,11 +25,13 @@ cloudinary.config({
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB connection error:", err));
+  .then(() => {
+    console.log("✓ Connected to MongoDB");
+  })
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+  });
 
-.then(() => console.log("✓ Connected to MongoDB"))
-.catch(err => console.error("MongoDB connection error:", err));
 
 
 const app = express();
